bcb45c83a20461013319326d567217a9b6eee9c5
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-18 10:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Select the output element
+const output = document.getElementById("output");
+
+// Create a function that returns a promise
+function sayHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Call the function and update the output element text when promise resolves
+sayHello().then((result) => {
+  output.innerText = result;
+});
